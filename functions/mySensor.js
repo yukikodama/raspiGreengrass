d04145c6f3337f8d727895319b9965b5a0f16850
yuki-kodama <kodama@ap-com.co.jp>
2019-07-01 20:10:54 +0900

    delete dynamodb

--- functions/mySensor.js
+++ functions/mySensor.js
@@ -19,49 +19,16 @@ function publishCallback(err, data) {
 }
 
 const myPlatform = util.format('%s-%s', os.platform(), os.release());
-const pubOpt = {
-    topic: 'topic/sensor',
-    payload: JSON.stringify({message: util.format('Hello world! Sent from Greengrass Core running on platform: %s using NodeJS, ID: %s', myPlatform, id)}),
-};
 
 function greengrassHelloWorldRun() {
     const now = new Date().getTime();
-    const pir = Number(d3.read());
     console.log("id:", id);
-    console.log("pir: ", pir);
-    console.log("CreateTime:", createTime);
+    console.log("createTime:", createTime);
     console.log("now: ", now);
-    const countup = {
-        TableName: "MyPirSensor",
-        Key: {"Id": id, "CreateTime": createTime},
-        UpdateExpression: "set During = During + :d, UpdateTime = :t",
-        ExpressionAttributeValues: {":d": 5000, ":t": now},
-        ReturnValues: "UPDATED_NEW"
+    const pubOpt = {
+        topic: 'topic/sensor',
+        payload: JSON.stringify({message: util.format('Sent from Greengrass Core running on platform: %s using NodeJS, ID: %s, createTime: %s, now: %s', myPlatform, id, createTime, now)}),
     };
-
-    docClient.update(countup, function (err, data) {
-        if (err) {
-            console.error(err);
-            const reset = {
-                TableName: "MyPirSensor",
-                Key: {"Id": id, "CreateTime": now},
-                UpdateExpression: "set During = :d, UpdateTime = :t",
-                ExpressionAttributeValues: {":d": 0, ":t": now},
-                ReturnValues: "UPDATED_NEW"
-            };
-            docClient.put(reset, function (err, data) {
-                if (err) {
-                    console.error(err);
-                    console.error("Unable to Put item. Error JSON:", JSON.stringify(err, null, 2));
-                } else {
-                    console.log("Put Item succeeded:", JSON.stringify(data, null, 2));
-                }
-            });
-            createTime = now;
-        } else {
-            console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
-        }
-    });
     iotClient.publish(pubOpt, publishCallback);
 }
 
