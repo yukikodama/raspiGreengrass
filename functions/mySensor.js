c43d9918612bbe96f40f441294249fdd279bebea
kodama <kodama@ap-com.co.jp>
2019-07-03 16:48:58 +0900

    DynamoDB code

--- functions/mySensor.js
+++ functions/mySensor.js
@@ -4,22 +4,15 @@ const ggSdk = require('aws-greengrass-core-sdk');
 const aws = require('aws-sdk');
 const docClient = new aws.DynamoDB.DocumentClient({region: "us-east-1"});
 const iotClient = new ggSdk.IotData();
-const os = require('os');
-const util = require('util');
-const uuid = require('node-uuid');
 const grovePi = require('node-grovepi').GrovePi;
 const digital = grovePi.sensors.base.Digital;
-const id = uuid.v4().split('-').join('');
-const serial = require('proc-cpuinfo')()["Serial"][0];
-
-var createTime = new Date().getTime();
+const sensorId = require('proc-cpuinfo')()["Serial"][0];
+const createAt = new Date().getTime();
 
 function publishCallback(err, data) {
     console.log(err);
     console.log(data);
 }
-
-const myPlatform = util.format('%s-%s', os.platform(), os.release());
 
 const board = new grovePi.board({
     debug: true,
@@ -29,25 +22,24 @@ const board = new grovePi.board({
     onInit: function (res) {
         const dPir = new digital(3);
         const params = {
-            TableName: "MySensor",
-            Key: {"Id": id, "Sensor": serial},
+            TableName: "MyPirSensor",
+            Key: {"SensorId": sensorId, "CreateAt": createAt},
         };
         setInterval(function () {
             const now = new Date().getTime();
             var pir = Number(dPir.read());
-            const message = {Id :id, Sensor: serial, During : 0, Pir: pir, CreateTIme: createTime, UpdateTime: now};
+            const message = {SensorId: sensorId, CreateAt: createAt, During: 0, Pir: pir, UpdateTime: now};
             if (pir) {
 
             } else {
                 const v = {
-                    TableName: "MySensor",
+                    TableName: "MyPirSensor",
                     Item: message
                 };
-                docClient.put(v, function(err, data) {
+                docClient.put(v, function (err, data) {
                     if (err) {
                         console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
-                    }
-                    else {
+                    } else {
                         console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
                     }
                 });
@@ -55,7 +47,6 @@ const board = new grovePi.board({
             const pubOpt = {
                 topic: 'topic/sensor',
                 payload: JSON.stringify(message)
-                // payload: JSON.stringify({message: util.format('Sent from Greengrass Core running on platform: %s using NodeJS, Id: %s, serial: %s, createTime: %s, now: %s, pir: %s', myPlatform, id, serial, createTime, now, pir)})
             };
             iotClient.publish(pubOpt, publishCallback);
         }, 5000);
