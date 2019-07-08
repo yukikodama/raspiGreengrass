338c2387135810bd35d99602f5d1350dd860859d
yuki-kodama <kodama@ap-com.co.jp>
2019-07-08 18:28:16 +0900

    error log fix.

--- functions/mySensor.js
+++ functions/mySensor.js
@@ -10,14 +10,14 @@ const sensorId = require('proc-cpuinfo')()["Serial"][0];
 const createAt = new Date().getTime();
 
 function publishCallback(err, data) {
-    console.error(err);
-    console.log(data);
+    console.error("publishCallback error: ", err);
+    console.log("publishCallback data: ", data);
 }
 
 const board = new grovePi.board({
     debug: true,
     onError: function (err) {
-        console.error(JSON.stringify(err, null, 2));
+        console.error("onError: ", JSON.stringify(err, null, 2));
     },
     onInit: function (res) {
         const digitalSensor = new digital(3);
@@ -45,9 +45,9 @@ const board = new grovePi.board({
                 const v = {TableName: "PirSensor", Item: message};
                 docClient.put(v, function (err, data) {
                     if (err) {
-                        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+                        console.error("Unable to put item. Error JSON:", JSON.stringify(err, null, 2));
                     } else {
-                        console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+                        console.log("PutItem succeeded:", JSON.stringify(data, null, 2));
                     }
                 });
             }
