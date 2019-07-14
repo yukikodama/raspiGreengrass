187c2e2c33e394df4d7a0b92f02196c9bafbcf44
yukikodama <Courage123456!>
2019-07-14 14:34:57 +0900

    CF code.

--- functions/conferenceRoomSensor.js
+++ functions/conferenceRoomSensor.js
@@ -4,10 +4,15 @@ const ggSdk = require('aws-greengrass-core-sdk');
 const aws = require('aws-sdk');
 const docClient = new aws.DynamoDB.DocumentClient({region: "us-east-1"});
 const iotClient = new ggSdk.IotData();
-const grovePi = require('node-grovepi').GrovePi;
-const digital = grovePi.sensors.base.Digital;
+const GrovePi = require('node-grovepi').GrovePi;
 const sensorId = require('proc-cpuinfo')()["Serial"][0];
 const createAt = new Date().getTime();
+
+const a0 = new GrovePi.sensors.base.Analog(0);
+const a1 = new GrovePi.sensors.base.Analog(1);
+const d2 = new GrovePi.sensors.DigitalInput(2);
+const d3 = new GrovePi.sensors.DigitalOutput(3);
+const d4 = new GrovePi.sensors.DigitalOutput(4);
 
 const publishCallback = async (err, data) => {
     console.error("publishCallback error: ", await err);
@@ -15,20 +20,27 @@ const publishCallback = async (err, data) => {
 }
 
 const onErrorFunction = async (err) => {
-    console.error("onError: ", JSON.stringify(err, null, 2));
+    console.error("onError: ", JSON.stringify(await err, null, 2));
 }
 
 const onInitFunction = async () => {
-    const digitalSensor = new digital(3);
-    setInterval(function(){intervalExecute(digitalSensor)}, 1000);
+    setInterval(function(){intervalExecute()}, 1000);
 }
 
-const intervalExecute = async (digitalSensor) => {
+const intervalExecute = async () => {
+    const light = Number(a0.read());
+    const sound = Number(a1.read());
+    const pir   = Number(d2.read());
     const updateAt = new Date().getTime();
-    const pir = Number(digitalSensor.read());
-    console.log(await pir);
-
+    const message = {SensorId: sensorId, UpdateAt: updateAt, During: 0, Light: light, Sound: sound, Pir: pir, CreateAt: createAt, };
+    const pubOpt = {
+        topic: 'topic/cfRoomSensor',
+        payload: JSON.stringify(message)
+    };
+    console.log("message: ", await message);
+    iotClient.publish(pubOpt, publishCallback);
 }
+
 const board = new grovePi.board({
     debug: true,
     onError: onErrorFunction,
