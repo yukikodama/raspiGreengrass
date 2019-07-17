54b6c1fcaf16db623c365ee246ae1d8e18cca0bf
kodama <kodama@ap-com.co.jp>
2019-07-17 17:26:25 +0900

    change api.

--- index.js
+++ index.js
@@ -3,11 +3,50 @@
 const aws = require('aws-sdk');
 const docClient = new aws.DynamoDB.DocumentClient({region: "us-east-1"});
 
+const docClientFunction = async(params, scan = false) => {
+    return new Promise(async(resolve, reject) => {
+        if (scan) {
+            docClient.scan(params, async(err, data) => {
+                resolve(await data.Items);
+        });
+        }
+        else {
+            docClient.query(params, async(err, data) => {
+                resolve(await data.Items);
+            });
+        }
+    });
+};
+
+const cfRoomSensorParams = {
+    TableName: 'cfRoomSensor',
+    KeyConditionExpression: "#si = :s",
+    ExpressionAttributeNames: {
+        "#si": "SensorId"
+    },
+    ExpressionAttributeValues: {},
+    ScanIndexForward: false,
+    Limit: 5
+};
+
 exports.handler = (event, context, callback) => {
     const done = (err, res) => callback(null, {
         statusCode: err ? '400' : '200',
         body: err ? err.message : JSON.stringify(res),
         headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
     });
-    docClient.scan({TableName : 'PirSensor'}, done);
+    const s = await docClientFunction({ TableName: 'Sensor' }, true);
+    console.info("s: ", s);
+    var array = [];
+    for (var i of s) {
+        cfRoomSensorParams.ExpressionAttributeValues = { ":s": i.SensorId };
+        const item = await docClientFunction(cfRoomSensorParams);
+        array = array.concat(item);
+    }
+    console.info("array: ", array);
+    callback(null, {
+        statusCode: '200',
+        body:  JSON.stringify(array),
+        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+    });
 }
