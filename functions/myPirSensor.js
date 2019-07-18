0a2dfea169e98d1636a31de21b63062059f1c8b6
yukikodama <Courage123456!>
2019-07-19 00:20:56 +0900

    add pirSensor.

--- functions/myPirSensor.js
+++ functions/myPirSensor.js
@@ -4,64 +4,60 @@ const ggSdk = require('aws-greengrass-core-sdk');
 const aws = require('aws-sdk');
 const docClient = new aws.DynamoDB.DocumentClient({region: "us-east-1"});
 const iotClient = new ggSdk.IotData();
-const grovePi = require('node-grovepi').GrovePi;
-const digital = grovePi.sensors.base.Digital;
+const GrovePi = require('node-grovepi').GrovePi;
 const sensorId = require('proc-cpuinfo')()["Serial"][0];
 const createAt = new Date().getTime();
 
-function publishCallback(err, data) {
-    console.error("publishCallback error: ", err);
-    console.log("publishCallback data: ", data);
+
+const a0 = new GrovePi.sensors.base.Analog(0);
+const d2 = new GrovePi.sensors.DigitalInput(2);
+
+const publishCallback = async (err, data) => {
+    console.error("publishCallback error: ", await err);
+    console.log("publishCallback data: ", await data);
 }
 
-const board = new grovePi.board({
+const onErrorFunction = async (err) => {
+    console.error("onError: ", JSON.stringify(await err, null, 2));
+}
+
+const onInitFunction = async () => {
+    const sensor = {TableName: "Sensor", Item: {SensorId: sensorId}};
+    docClient.put(sensor, function (err, data) {
+        console.log("Put Sensor succeeded:");
+    });
+    setInterval(function(){intervalExecute()}, 10000);
+}
+
+const intervalExecute = async () => {
+    const light = Number(a0.read());
+    const pir   = Number(d2.read());
+    const updateAt = new Date().getTime();
+    const message = {SensorId: sensorId, UpdateAt: updateAt, During: 0, Light: light, Pir: pir, CreateAt: createAt};
+    const putItem = {TableName: "PirSensor", Item: message};
+    const pubOpt = {
+        topic: 'topic/pirSensor',
+        payload: JSON.stringify(message)
+    };
+    docClient.put(putItem, function (err, data) {
+        if (err) {
+            console.error("Unable to put item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("PutItem succeeded:", JSON.stringify(data, null, 2));
+        }
+    });
+    iotClient.publish(pubOpt, publishCallback);
+}
+
+const board = new GrovePi.board({
     debug: true,
-    onError: function (err) {
-        console.error("onError: ", JSON.stringify(err, null, 2));
-    },
-    onInit: function (res) {
-        const digitalSensor = new digital(3);
-        setInterval(function () {
-            const updateAt = new Date().getTime();
-            const pir = Number(digitalSensor.read());
-            const message = {SensorId: sensorId, During: 0, Pir: pir, CreateAt: createAt, UpdateAt: updateAt};
-            if (pir) {
-                const up = {
-                    TableName: "PirSensor",
-                    Key: {SensorId: sensorId},
-                    UpdateExpression: "set During = During + :d, Pir = :p, CreateAt = :c, UpdateAt = :u",
-                    ExpressionAttributeValues: {":d": 5000, ":p": pir, ":c": createAt, ":u": updateAt},
-                    ReturnValues: "UPDATED_NEW"
-                };
-                docClient.update(up, function (err, data) {
-                    if (err) {
-                        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
-                    } else {
-                        console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
-                        message.During = data.Attributes.During;
-                    }
-                });
-            } else {
-                const v = {TableName: "PirSensor", Item: message};
-                docClient.put(v, function (err, data) {
-                    if (err) {
-                        console.error("Unable to put item. Error JSON:", JSON.stringify(err, null, 2));
-                    } else {
-                        console.log("PutItem succeeded:", JSON.stringify(data, null, 2));
-                    }
-                });
-            }
-            const pubOpt = {
-                topic: 'topic/sensor',
-                payload: JSON.stringify(message)
-            };
-            iotClient.publish(pubOpt, publishCallback);
-        }, 5000);
-    }
+    onError: onErrorFunction,
+    onInit: onInitFunction
 });
 board.init();
 
-exports.handler = function handler(event, context) {
+exports.handler = async (event, context, callback) => {
     console.log("event: ", event);
     console.log("context:", context);
-};
+    callback(null, 'ok')
+}
